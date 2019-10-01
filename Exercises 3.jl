7d033589933483a649c00733081a642a14496361
Cameron <calaha717@yahoo.com>
2019-10-01 18:40:52 -0400

    Added extract() function [answer to 1b]

--- Exercises 3.jl
+++ Exercises 3.jl
@@ -1,2 +1,14 @@
 using PyPlot; pygui(true);
 using JLD; using Statistics;
+
+img = imread("el-capitan.png")
+imshow(img) # Answer to question 1a.
+
+function extract(image_file) # Answer to question 1b.
+    img = imread(image_file)
+    imshow(img)
+    red = img[:,:,1] # Gets red channel.
+    green = img[:,:,2] # Gets green channel.
+    blue = img[:,:,3] # Gets blue channel.
+    return red, green, blue
+end
